ee5ecd24e9969aedc832ace961b1b5c1d2d9298b
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-06 17:39:36 +0100

    MVP done, started extension

--- room.rb
+++ room.rb
@@ -1,11 +1,13 @@
 class Room
 
-def initialize(room_name, guest_count, till, song)
+def initialize(room_name, guest, till, song, room_capacity)
 
 @room_name = room_name
-@guest_count = guest_count
+@guest = []
 @till = till
-@song = song
+@song = []
+@room_capacity = room_capacity
+
 
 end
 
@@ -14,10 +16,35 @@ return @room_name
 end
 
 def guest_count()
-  return @guest_count
+  return @guest.count
 end
 
 def till_count()
 return @till
+end
+
+def check_in(guest)
+  if @room.guest_count < @room_capacity
+@guest.push(guest)
+end
+
+def check_out(guest)
+@guest.delete(guest)
+end
+
+def song()
+return @song.count
+end
+
+def add_song(song)
+@song.push(song)
+end
+
+def room_full?(room)
+if @room_capacity == room.guest_count()
+  return true
+else
+  return false
+end
 end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,21 +6,45 @@ require_relative("../song.rb")
 class RoomTest < MiniTest::Test
 
 def setup()
-  @guest = Guest.new("Dave", 50)
-@song = Song.new("Monkey Wrench", "Foo Fighters")
-  @room = Room.new("Room 1", 0, 100, @song)
+  @guest1 = Guest.new("Dave", 50)
+  @guest2 = Guest.new("Jordyn", 60)
+  @guest3 = Guest.new("Virginia", 40)
+  @guest = [@guest1, @guest2, @guest3]
+@song1 = Song.new("Monkey Wrench", "Foo Fighters")
+@song2 = Song.new("Song 2", "Blur")
+@song = [@song1, @song2]
+  @room = Room.new("Room 1", @guest, 100, @song, 3)
 
 end
 
-def test_room_name
+def test_room_name()
   assert_equal("Room 1", @room.name)
 end
 
-def test_guest_count
+def test_guest_count()
 assert_equal(0, @room.guest_count)
 end
 
-def test_till_count
+def test_till_count()
 assert_equal(100, @room.till_count)
+end
+
+def test_guest_checks_in()
+  @room.check_in(@guest1)
+assert_equal(1, @room.guest_count)
+end
+
+def test_guest_checks_out()
+  @room.check_out(@guest1)
+  assert_equal(0, @room.guest_count)
+end
+
+def test_room_has_songs__added()
+  @room.add_song(@song2)
+assert_equal(1, @room.song)
+end
+
+def test_room_capacity_full_return_true()
+assert.equal(true, @room.room_full?())
 end
 end
