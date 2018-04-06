2f4556d1a4a6a68002756e3a0763c9ca3523114f
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-06 12:22:52 +0100

    room set up

---
+++ room.rb
@@ -0,0 +1,13 @@
+class Room
+
+def initialize(room_name, guest_count, till, song)
+
+@room_name = room_name
+@guest_count = guest_count
+@till = till
+@song = song
+
+end
+
+
+end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -5,6 +5,10 @@ require_relative("../guest.rb")
 require_relative("../song.rb")
 class RoomTest < MiniTest::Test
 
+def setup()
+@song = Song.new ("Monkey Wrench", "Foo Fighters")
+  @room = Room.new("Room 1", 0, 100, @song)
 
+end
 
 end
