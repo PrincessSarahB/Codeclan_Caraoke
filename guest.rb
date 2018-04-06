101cd1a5b5ef232dbca0576b89d0df65be5c462d
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-06 12:16:05 +0100

    guest created

---
+++ guest.rb
@@ -0,0 +1,12 @@
+class Guest
+
+def initialize(name, money, song)
+
+@name = name
+@money = money
+@song = song 
+
+end
+
+
+end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -1,9 +1,13 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../guest.rb")
-
+require_relative("../song.rb")
 class GuestTest < MiniTest::Test
 
+def setup()
+@fav_song = Song.new("Monkey Wrench", "Foo Fighters")
+@guest = Guest.new("Dave", 50, @fav_song)
 
+end
 
 end
