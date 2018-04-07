e27ec22268687146b81fb28927cdb4bef06a13c9
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-07 19:17:40 +0100

    bar has drinks and till

--- bar.rb
+++ bar.rb
@@ -1,10 +1,10 @@
 class Bar
 
-attr_reader :drink
-def initialize(drink)
+attr_reader :drink, :bar_till
+def initialize(drink, bar_till)
 
   @drink = drink
-
+@bar_till = bar_till
 end
 
 end
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -3,6 +3,7 @@ require("minitest/rg")
 require_relative("../bar.rb")
 require_relative("../room.rb")
 require_relative("../guest.rb")
+require_relative("../drinks.rb")
 
 class BarTest < MiniTest::Test
 
@@ -10,12 +11,16 @@ class BarTest < MiniTest::Test
     @drink1 = Drink.new("vodka", 5.00)
     @drink2 = Drink.new("wine", 4.00)
     @drink3 = Drink.new("beer", 3.00)
-  @drink = []
+  @drink = [@drink1, @drink2]
       @bar = Bar.new(@drink, 100)
 
     end
 
 def test_bar_has_drinks
-assert_equal()
+assert_equal(2, @drink.count)
+end
+
+def test_bar_has_till
+assert_equal(100, @bar.bar_till)
 end
   end
