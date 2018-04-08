921024d32242b0774b1765f618e790234455c153
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-08 15:22:45 +0100

    bar can serve drinks

--- bar.rb
+++ bar.rb
@@ -1,15 +1,18 @@
 class Bar
 
-attr_reader :drink, :bar_till
-def initialize(drink, bar_till)
+attr_reader :drink
+attr_accessor :bar_till, :guest
+def initialize(drink, bar_till, guest)
 
   @drink = drink
 @bar_till = bar_till
+@guest = guest
+
 end
 
-def can_buy_drink(drink, guest)
-  for drink in @drink
-if drink.price <= guest.money
+def can_buy_drink(guest)
+  for this_drink in @drink
+    if this_drink.price <= guest.money
 return true
 else
   return false
@@ -18,8 +21,22 @@ end
 end
 
 def find_drink_by_name(name)
-result = @drink.find {|drink| drink.name == name}
-return result.name
+if result = @drink.find {|drink| drink.name == name}
+return true
+else
+  return false
 end
+end 
+
+def serve(drink, guest)
+  if find_drink_by_name(drink.name) && can_buy_drink(guest)
+    @bar_till += drink.price
+    guest.money -= drink.price
+  return true
+else
+  return false
+end
+end
+
 
 end
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -8,13 +8,13 @@ require_relative("../drinks.rb")
 class BarTest < MiniTest::Test
 
   def setup()
-    @guest1 = Guest.new("Dave", 50, "Monkey Wrench")
+    @guest1 = Guest.new("Dave", 50.00, "Monkey Wrench")
     @guest2 = Guest.new("Harper", 4.00, "song")
     @drink1 = Drink.new("vodka", 5.00)
     @drink2 = Drink.new("wine", 4.00)
     @drink3 = Drink.new("beer", 3.00)
   @drink = [@drink1, @drink2]
-      @bar = Bar.new(@drink, 100)
+      @bar = Bar.new(@drink, 100.00, @guest)
 
     end
 
@@ -28,15 +28,25 @@ end
 
 
   def test_guest_can_buy_drink__has_money
-    assert_equal(true, @bar.can_buy_drink(@drink3, @guest1))
+    assert_equal(true, @bar.can_buy_drink(@guest1))
   end
 
   def test_guest_can_buy_drink__not_enough_money
 
-assert_equal(false, @bar.can_buy_drink(@drink1, @guest2))
+assert_equal(false, @bar.can_buy_drink(@guest2))
   end
 
   def test_find_drink_by_name
-assert_equal("vodka", @bar.find_drink_by_name("vodka"))
+assert_equal(true, @bar.find_drink_by_name("vodka"))
   end
+
+  def test_find_drink_by_name__drink_not_there
+assert_equal(false, @bar.find_drink_by_name("whisky"))
+  end
+
+def test_serve()
+  assert_equal(true, @bar.serve(@drink1, @guest1))
+  assert_equal(105.00, @bar.bar_till)
+  assert_equal(45, @guest1.money)
+end
   end
